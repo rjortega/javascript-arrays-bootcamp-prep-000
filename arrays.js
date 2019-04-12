afd363c50edadd9b6c7a135e87463614088bdf42
Ricardo Ortega <rikrdo.ortega@gmail.com>
2019-04-12 22:58:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
+  var array = [array]
   array.unshift[element]
   return array
 }
