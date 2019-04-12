a70a6f6053af130b498646cd7c3fd081dde7136e
Ricardo Ortega <rikrdo.ortega@gmail.com>
2019-04-12 22:33:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element){
-  var newArray = array.unshift(element)
+  var newArray = newarray.unshift(element)
   return newArray
 }
 
