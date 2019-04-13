8e611bfbaef69b2a9f865d50c6e2bd9600f74864
Ricardo Ortega <rikrdo.ortega@gmail.com>
2019-04-13 00:36:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,3 +38,8 @@ function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
   return array
 }
+
+function removeElementFromEndOfArray (array){
+  newestArray = array.slice(0, array.length - 1)
+  return newestArray
+}
